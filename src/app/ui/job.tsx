ab71a356f3cc96bb52ac4b325cd154bb4fda54d2
clopezochoa <carloslopezochoaaledo@gmail.com>
2024-03-14 10:49:42 +0100

    unify job component and select side via prop

--- src/app/ui/job.tsx
+++ src/app/ui/job.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import Icon from '../timeline/icon'
 
 export enum JobStatus {
   new = "new",
@@ -18,10 +17,11 @@ export interface Job {
   candidacies?: number,
   contactName?: string,
   contactLink?: string,
-
+  start?: boolean,
+  end?: boolean
 }
 
-export function JobStart(props: Job) {
+export function Job(props: Job) {
   const propsSecure = {
     date: props.date ?? "?",
     status: props.status ?? "?",
@@ -35,16 +35,13 @@ export function JobStart(props: Job) {
   }
   return (
     <li>
-      <div className="timeline-middle">
-        <Icon/>
-      </div>
-      <div className="timeline-start md:text-end mb-10">
+      <div className={`${props.start ? "timeline-start" : (props.end ? "timeline-end" : "timeline-middle")} timeline-box`}>
         <time className="font-mono italic">{propsSecure.date}</time>
         <div>
           <details className="collapse bg-base-200">
             <summary className="collapse-title text-lg font-black">
               <button className="btn btn-warning me-5">
-                <a href={propsSecure.link}
+                <a href={propsSecure.link !== "" ? propsSecure.link : undefined}
                   target="_blank"
                   rel="noopener noreferrer"
                 >
@@ -77,76 +74,6 @@ export function JobStart(props: Job) {
           </details>
         </div>
       </div>
-      <hr/>
-      <input type="checkbox" id="details" className="modal-toggle" />
-      <div className="modal" role="dialog">
-        <div className="modal-box">
-          <h3 className="text-lg font-bold">Job title</h3>
-          <p className="py-4">This are the details</p>
-        </div>
-        <label className="modal-backdrop" htmlFor="details">Close</label>
-      </div>
-    </li>
-  )
-}
-
-export function JobEnd(props: Job) {
-  const propsSecure = {
-    date: props.date ?? "?",
-    status: props.status ?? "?",
-    link: props.link ?? "",
-    company: props.company ?? "?",
-    title: props.title ?? "",
-    exp: props.exp?.toString() ?? "?",
-    candidacies: props.candidacies?.toString() ?? "?",
-    contactName: props.contactName ?? "",
-    contactLink: props.contactLink ?? "",
-  }
-  return (
-    <li>
-      <div className="timeline-middle">
-        <Icon/>
-      </div>
-      <div className="timeline-end md:text-start mb-10">
-        <time className="font-mono italic">{propsSecure.date}</time>
-        <div>
-          <details className="collapse bg-base-200">
-            <summary className="ms-4 collapse-title text-lg font-black">
-              {propsSecure.company}
-              <button className="btn btn-warning ms-5">
-                <a href={propsSecure.link}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  {propsSecure.status}
-                </a>
-              </button>
-            </summary>
-            <div className="collapse-content"> 
-              <ul>
-                <li>
-                  {propsSecure.title}
-                </li>
-                <li>
-                  {propsSecure.exp} Years of exp.
-                </li>
-                <li>
-                  {propsSecure.candidacies} solicitudes.
-                </li>
-                <li>
-                  <a href={propsSecure.contactLink}>
-                    {propsSecure.contactName}
-                  </a>
-                </li>
-                <li>
-                  <label htmlFor="details" className="btn">Details</label>
-                </li>
-              </ul>
-            </div>
-          </details>
-        </div>
-      </div>
-      <hr/>
       <input type="checkbox" id="details" className="modal-toggle" />
       <div className="modal" role="dialog">
         <div className="modal-box">
