23b2e36052672e081afd5c3dd2a48bfd439dadac
clopezochoa <carloslopezochoaaledo@gmail.com>
2024-03-14 15:11:21 +0100

    landing page embed tools

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,21 @@ import './style.css'
 import Navbar from "./ui/navbar";
 import Timeline from "./timeline/timeline";
 import PostModal from "./ui/post-modal";
+import Tools from "./ui/tools";
 
 export default function Home() {
   return (<>
+    <PostModal/>
     <Navbar/>
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <PostModal/>
-      <Timeline/>
+    <main className="flex min-h-screen items-start justify-between">
+      <div className="flex justify-start pt-20">
+        <Tools/>
+      </div>
+      <div className="flex min-h-screen flex-col items-center justify-between p-24">
+        <Timeline/>
+      </div>
+      <div className="flex justify-start pt-20 w-52">
+      </div>
     </main>
   </>
   );
