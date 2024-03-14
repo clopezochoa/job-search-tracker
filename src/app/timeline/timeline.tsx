cf800261fa34503f0246ef374e796f8299832b6f
clopezochoa <carloslopezochoaaledo@gmail.com>
2024-03-14 15:10:18 +0100

    time timeline mock

--- src/app/timeline/timeline.tsx
+++ src/app/timeline/timeline.tsx
@@ -1,18 +1,20 @@
 import React from 'react'
 import { Job } from '../ui/job'
+import { getMomentFormatted } from '@/app/lib';
 
 function Timeline() {
   const mockjobs = Array.from(Array(30));
+  const time = getMomentFormatted();
   return (<>
     <ul className="timeline timeline-vertical">
       {mockjobs.map((job, index) => {
         if(index % 2 === 0) {
           return (<>
-            <Job start date={new Date(Date.now()).toString().slice(0, 30)}/>
+            <Job start date={time}/>
           </>)
         } else {
           return (<>
-            <Job end date={new Date(Date.now()).toString().slice(0, 30)}/>
+            <Job end date={time}/>
           </>)
         }
       })}
