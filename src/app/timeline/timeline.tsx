5853dab86159fde48abcd1999db6b566b89310b2
clopezochoa <carloslopezochoaaledo@gmail.com>
2024-03-14 10:50:07 +0100

    timeline cleanup

--- src/app/timeline/timeline.tsx
+++ src/app/timeline/timeline.tsx
@@ -1,18 +1,18 @@
 import React from 'react'
-import { JobEnd, JobStart } from '../ui/job'
+import { Job } from '../ui/job'
 
 function Timeline() {
   const mockjobs = Array.from(Array(30));
   return (<>
-    <ul className="timeline timeline-snap-icon max-md:timeline-compact timeline-vertical">
+    <ul className="timeline timeline-vertical">
       {mockjobs.map((job, index) => {
         if(index % 2 === 0) {
           return (<>
-            <JobStart date={new Date(Date.now()).toString().slice(0, 30)}/>
+            <Job start date={new Date(Date.now()).toString().slice(0, 30)}/>
           </>)
         } else {
           return (<>
-            <JobEnd date={new Date(Date.now()).toString().slice(0, 30)}/>
+            <Job end date={new Date(Date.now()).toString().slice(0, 30)}/>
           </>)
         }
       })}
