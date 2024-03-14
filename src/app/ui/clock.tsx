c2162457ca5a7684096da3972f5801d0db56fb12
clopezochoa <carloslopezochoaaledo@gmail.com>
2024-03-14 15:10:00 +0100

    fix clock hydration issue

--- src/app/ui/clock.tsx
+++ src/app/ui/clock.tsx
@@ -1,24 +1,22 @@
 'use client'
 
-import moment from 'moment';
 import React, { useEffect, useState } from 'react';
-import { GetServerSideProps } from 'next';
+import { getMomentFormatted } from '@/app/lib';
+import { useIsClient } from '../providers/context';
 
-const Clock = () => {
-  const [time, setTime] = useState("");
+export const Clock = () => {
+  const client = useIsClient();
+  const initTime = getMomentFormatted();
+  const [time, setTime] = useState(initTime);
   useEffect(() => {
     const interval = setInterval(() => {
       setTime(
-        `${moment().format('dddd')} ${moment().format('Do')} ${moment().format('MMMM')} ${moment().format('h:mm:ss a')}`
+        getMomentFormatted()
       )
     }, 1000);
     return () => clearInterval(interval);
   }, []);
   return (
-    <div>
-      <span>{time}</span>
-    </div>
-  );
+    <span>{client ? time : ""}</span>
+  )
 };
-
-export default Clock;
