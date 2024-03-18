6f450703a4daa2e57412f567254626b147106d9b
clopezochoa <carloslopezochoaaledo@gmail.com>
2024-03-18 10:47:11 +0100

    modal full screen utility component

---
+++ src/app/ui/modal.tsx
@@ -0,0 +1,21 @@
+import React, { ReactNode, useEffect } from 'react'
+import { CloseIcon } from '../icons';
+
+function Modal({children, hide}:{children:ReactNode, hide: () => void}) {
+  useEffect(() => {
+    document.body.style.overflow = "hidden";
+    return () => {document.body.style.overflow = "auto"};
+  }, []);
+  return (
+    <div style={{position:"absolute", zIndex:"10", width:"100%", height:"100%", backgroundColor:"black"}}>
+      <div onClick={hide} style={{cursor:"pointer", position:"absolute", zIndex:"11", top:"15px", right:"15px"}}>
+        {CloseIcon}
+      </div>
+      <center>
+        {children}
+      </center>
+    </div>
+  )
+}
+
+export default Modal
