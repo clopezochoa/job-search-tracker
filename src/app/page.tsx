cb6beccdecf9f1ead07a1102618ad632f7f75324
clopezochoa <carloslopezochoaaledo@gmail.com>
2024-03-19 08:15:49 +0100

    cleanup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import './style.css'
 import Navbar from "./ui/navbar";
-import PostModal from "./ui/post-modal";
 import Main from './ui';
 
 export default function Home() {
--- src/app/ui/navbar.tsx
+++ src/app/ui/navbar.tsx
@@ -3,7 +3,6 @@
 import React, { useState } from 'react'
 import {Clock} from './clock';
 import { CookieSymbol, createSession, useIsClient, useSession } from '../providers/context';
-import { Login, Signup } from './auth';
 import { useCookies } from 'react-cookie';
 import { ToolsToggle } from './tools';
 import PostModal from './post-modal';
@@ -11,9 +10,6 @@ import PostModal from './post-modal';
 function Navbar() {
   const client = useIsClient();
   const sessionCtx = useSession();
-  const [toggleSignup, setToggleSignup] = useState(false);
-  const [toggleLogin, setToggleLogin] = useState(false);
-  const [togglePost, setTogglePost] = useState(false);
   const cookies = useCookies([CookieSymbol.session]);
   const removeCookie = cookies[2];
 
@@ -24,13 +20,6 @@ function Navbar() {
   }
 
   return(<>
-    {
-      client ? <>
-      {toggleSignup ? <Signup hide={() => setToggleSignup(false)} /> : null}
-      {toggleLogin ? <Login hide={() => setToggleLogin(false)} /> : null}
-      {togglePost ? <PostModal hide={() => setTogglePost(false)}/> : null}
-      </> : <></>
-    }
     <div className="navbar navbar-custom bg-base-300 z-[1] flex justify-between">
       <div className="px-2 min-w-96">
       {client && sessionCtx?.session.isSession ? 
@@ -38,34 +27,15 @@ function Navbar() {
       :<></>}
       </div>
       <div className="px-2 grid text-center">
-        <span className='text-xl subpixel-antialiased'>NOW</span>
         <Clock/>
       </div>
-      <div className="flex justify-end px-4 min-w-96">
+      <div className="flex justify-end px-6 min-w-96">
         <div className="flex items-stretch">
           {
-            client && sessionCtx?.session.isSession ? 
-            <button onClick={handleLogout} className="btn btn-ghost rounded-btn">Logout</button> :
-            <button onClick={() => setToggleSignup(toggleSignup => !toggleSignup)} className="btn btn-ghost rounded-btn">Signup</button>
+            client && sessionCtx?.session.isSession ?
+            <button onClick={handleLogout} className="btn btn-ghost rounded-btn">Logout</button>
+            : null
           }
-          <div className="dropdown dropdown-end ms-2">
-            {
-              client && sessionCtx?.session.isSession ? 
-              <>
-                <div tabIndex={0} role="button" className="btn btn-ghost rounded-btn">Menu</div>
-                <ul tabIndex={0} className="menu dropdown-content z-[1] p-2 shadow bg-base-100 rounded-box w-52 mt-4">
-                  <li><button onClick={() => setTogglePost(true)} className="btn">Post a job</button></li>
-                  <li><a href='https://www.lopezochoa.com' target="_blank" rel="noopener noreferrer">Portfolio</a></li>
-                  <li><a>Linktree</a></li>
-                  <li><a>Linkedin</a></li>
-                  <li><a>Manfred</a></li>
-                  <li><a>Wellfound</a></li>
-                  <li><a>Joinrs</a></li>
-                </ul>
-              </> :
-              <button onClick={() => setToggleLogin(toggleLogin => !toggleLogin)} className="btn btn-ghost rounded-btn">Login</button>
-            }
-          </div>
         </div>
       </div>
     </div>
