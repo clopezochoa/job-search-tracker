0b76a60d0beda95884970a2771a6cc254f96ea75
clopezochoa <carloslopezochoaaledo@gmail.com>
2024-03-19 08:45:30 +0100

    Format right panel items

--- src/app/icons.tsx
+++ src/app/icons.tsx
@@ -29,3 +29,15 @@ export const CompanyIcon = <svg xmlns="http://www.w3.org/2000/svg" fill="none" v
 export const RoleIcon = <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
 <path strokeLinecap="round" strokeLinejoin="round" d="M4.26 10.147a60.438 60.438 0 0 0-.491 6.347A48.62 48.62 0 0 1 12 20.904a48.62 48.62 0 0 1 8.232-4.41 60.46 60.46 0 0 0-.491-6.347m-15.482 0a50.636 50.636 0 0 0-2.658-.813A59.906 59.906 0 0 1 12 3.493a59.903 59.903 0 0 1 10.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.717 50.717 0 0 1 12 13.489a50.702 50.702 0 0 1 7.74-3.342M6.75 15a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Zm0 0v-3.675A55.378 55.378 0 0 1 12 8.443m-7.007 11.55A5.981 5.981 0 0 0 6.75 15.75v-1.5" />
 </svg>;
+
+export const LinkIcon = <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+<path strokeLinecap="round" strokeLinejoin="round" d="M13.19 8.688a4.5 4.5 0 0 1 1.242 7.244l-4.5 4.5a4.5 4.5 0 0 1-6.364-6.364l1.757-1.757m13.35-.622 1.757-1.757a4.5 4.5 0 0 0-6.364-6.364l-4.5 4.5a4.5 4.5 0 0 0 1.242 7.244" />
+</svg>;
+
+export const ArrowUpLeftIcon = <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+<path strokeLinecap="round" strokeLinejoin="round" d="m19.5 19.5-15-15m0 0v11.25m0-11.25h11.25" />
+</svg>;
+
+export const DocumentIcon = <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+<path strokeLinecap="round" strokeLinejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m2.25 0H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z" />
+</svg>;
--- src/app/ui/index.tsx
+++ src/app/ui/index.tsx
@@ -6,6 +6,7 @@ import { Tools } from './tools';
 import { Timeline } from '../timeline/timeline';
 import Landing from './landing';
 import PostModal from './post-modal';
+import { ArrowUpLeftIcon, DocumentIcon } from '../icons';
 
 function Main() {
   const session = useSession();
@@ -23,12 +24,21 @@ function Main() {
           <div className="flex min-h-screen flex-col items-center justify-between p-24">
             <Timeline/>
           </div>
-          <div className="grid justify-end pt-16 min-w-96 mt-4 mr-4 fixed top-0 right-0">
-            <button onClick={() => setTogglePost(true)} className="btn justify-end w-fit self-end">Post a job</button>
-            <ul tabIndex={0} className="right-panel p-2 rounded-box min-w-60 justify-end text-end">
-              <li><a href='https://www.lopezochoa.com' target="_blank" rel="noopener noreferrer">Portfolio</a></li>
-              <li><a>Linktree</a></li>
-              <li><a>Linkedin</a></li>
+          <div className="grid justify-end pt-16 mt-4 mr-4 fixed top-0 right-0">
+            <button onClick={() => setTogglePost(true)} className="btn btn-primary ">Post a job</button>
+            <ul tabIndex={0} className="[&>*]:mr-2 [&>*]:mt-2 p-2 rounded-box justify-end text-end">
+              <li><h2 className='font-bold'>Documents</h2></li>
+              <li><a className='flex justify-end [&>*]:ms-2' href='https://www.lopezochoa.com' target="_blank" rel="noopener noreferrer">{ArrowUpLeftIcon}<span>Portfolio</span></a></li>
+              <li><a className='flex justify-end [&>*]:ms-2'>{DocumentIcon}<span>CV</span></a></li>
+              <li className='flex justify-end [&>*]:ms-2'>
+                <a href='https://www.linktr.ee/clopezochoa' target="_blank" rel="noopener noreferrer">{ArrowUpLeftIcon}</a>
+                <span style={{cursor:"pointer"}} onClick={() => {navigator.clipboard.writeText("https://www.linktr.ee/clopezochoa")}}>Linktree</span>
+              </li>
+              <li className='flex justify-end [&>*]:ms-2'>
+                <a href='https://www.linkedin.com/in/cloa' target="_blank" rel="noopener noreferrer">{ArrowUpLeftIcon}</a>
+                <span style={{cursor:"pointer"}} onClick={() => {navigator.clipboard.writeText("https://www.linkedin.com/in/cloa")}}>LinkedIn</span>
+              </li>
+              <li><h2 className='font-bold'>Providers</h2></li>
               <li><a>Manfred</a></li>
               <li><a>Wellfound</a></li>
               <li><a>Joinrs</a></li>
