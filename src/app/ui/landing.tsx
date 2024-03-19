e1ee9f90ae6b002c11b1c1cb388d68eb4bb0f711
clopezochoa <carloslopezochoaaledo@gmail.com>
2024-03-19 08:15:13 +0100

    move authentication to landing

--- src/app/ui/landing.tsx
+++ src/app/ui/landing.tsx
@@ -1,14 +1,12 @@
-'use client'
-
 import React, { useRef, useState } from 'react'
-import { EmailIcon, KeyIcon } from '../icons'
-import Modal from './modal'
 import { CookieSymbol, createSession, createSettings, useSession } from '../providers/context';
 import { useCookies } from 'react-cookie';
 import { UserData } from '../client-lib';
+import { EmailIcon, KeyIcon } from '../icons';
 import { genSaltSync, hashSync } from 'bcrypt-ts';
 
-export function Login({hide}:{hide:() => void}) {
+function Landing() {
+
   const sessionContext = useSession();
   const [cookie, setCookie] = useCookies([CookieSymbol.session]);
 
@@ -43,7 +41,6 @@ export function Login({hide}:{hide:() => void}) {
             sameSite: "strict",
           });
           sessionContext?.updateSession(createSession(true, user));
-          hide();
         } else {
           throw new Error("Error while authenticating.")
         }
@@ -52,45 +49,6 @@ export function Login({hide}:{hide:() => void}) {
       console.error(error)
     }
   }
-
-  const [passwordVisible, setPasswordVisible] = useState(false);
-
-  const email = {
-    jsx: <input ref={emailRef} required type="text" className="grow" placeholder="Email" />,
-    icon: EmailIcon
-  }
-  const password = {
-    jsx: <input ref={passwordRef} required type={passwordVisible ? "text" : "password"} className="grow" placeholder='Password'/>,
-    icon: <i onClick={() => setPasswordVisible(passwordVisible => !passwordVisible)}>{KeyIcon}</i>
-  }
-
-  const formItem = (item: {jsx: React.JSX.Element, icon: React.JSX.Element}) => {
-    return (
-      <>
-        <label className="input input-bordered flex items-center gap-2">
-          {item.jsx}
-          {item.icon}
-        </label>
-      </>
-    )
-  }
-  return (
-    <Modal hide={hide}>
-      <div style={{position: "absolute", top:"25%", right:"25%", width:"50%", height:"50%", display:"grid", alignItems:"space-around"}}>
-        {formItem(email)}
-        {formItem(password)}
-        <button onClick={handleLogin} className="btn btn-neutral">Login</button>
-      </div>
-    </Modal>
-  )
-}
-
-export function Signup({hide}:{hide:() => void}) {
-  const sessionContext = useSession();
-  const [cookie, setCookie] = useCookies([CookieSymbol.session]);
-  const nameRef = useRef(null);
-  const emailRef = useRef(null);
-  const passwordRef = useRef(null);
 
   const handleSignup = async (e: any) => {
     e.preventDefault();
@@ -103,15 +61,14 @@ export function Signup({hide}:{hide:() => void}) {
     }
     
     try {
-      if(emailRef.current && passwordRef.current && nameRef.current) {
+      if(emailRef.current && passwordRef.current) {
         const settings = createSettings();
         const key = genSaltSync(4);
-        const name = (nameRef.current as HTMLInputElement).value;
         const email = (emailRef.current as HTMLInputElement).value;
         var password = (passwordRef.current as HTMLInputElement).value;
         const salt = genSaltSync(10);
         password = hashSync(password, salt);  
-        const body = {key, name, email, password, settings} as UserData;
+        const body = {key, email, password, settings} as UserData;
         const token = await fetch('/api/signup', {
           method: 'POST',
           headers: { 'Content-Type': 'application/json' },
@@ -125,7 +82,6 @@ export function Signup({hide}:{hide:() => void}) {
             sameSite: "strict",
           });
           sessionContext?.updateSession(createSession(true, body));
-          hide();
         }
       } else {
         throw new Error("No email or password HTML element was found.")
@@ -136,10 +92,7 @@ export function Signup({hide}:{hide:() => void}) {
   }
 
   const [passwordVisible, setPasswordVisible] = useState(false);
-  const name = {
-    jsx: <input ref={nameRef} type="text" className="grow" placeholder="Name" />,
-    icon: <></>
-  }
+
   const email = {
     jsx: <input ref={emailRef} required type="text" className="grow" placeholder="Email" />,
     icon: EmailIcon
@@ -159,14 +112,42 @@ export function Signup({hide}:{hide:() => void}) {
       </>
     )
   }
+
   return (
-    <Modal hide={hide}>
-      <div style={{position: "absolute", top:"25%", right:"25%", width:"50%", height:"50%", display:"grid", alignItems:"space-around"}}>
-        {formItem(name)}
-        {formItem(email)}
-        {formItem(password)}
-        <button onClick={handleSignup} className="btn btn-neutral">Login</button>
+    <main>
+      <div className="hero min-h-screen bg-base-200">
+        <div className="hero-content flex-col lg:flex-row-reverse" style={{alignItems:"baseline"}}>
+          <div className="text-center lg:text-left  ms-4">
+            <h1 className="text-5xl font-bold">Welcome!</h1>
+            <p className="py-6">Track your candidacies today, get the job you deserve tomorrow.</p>
+          </div>
+          <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+            <form className="card-body">
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                {formItem(email)}
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                {formItem(password)}
+                <label className="label">
+                  <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                </label>
+              </div>
+              <div className="form-control mt-6">
+                <button onClick={(e) => handleLogin(e)} className="btn btn-primary">Login</button>
+                <button onClick={(e) => handleSignup(e)} className="btn btn-secondary mt-3">Signup</button>
+              </div>
+            </form>
+          </div>
+        </div>
       </div>
-    </Modal>
+    </main>
   )
 }
+
+export default Landing
